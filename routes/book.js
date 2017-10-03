afb20dde39cc1b4549425fc296fb6e6791bebdd2
KevinMAnderson <kma2k4@hotmail.com>
2017-10-03 13:09:11 -0600

    modified book route

--- routes/book.js
+++ routes/book.js
@@ -6,10 +6,28 @@ const knex = require('../knex');
 // knex.destroy();
 
 /* GET all BOOK LISTING. */
+
+//TODO add authors to this response
 router.get('/', function(request, response, next) {
-    knex('book').then(book =>{
-       response.json(book);
+
+    // get book data
+
+    knex('book').select('book_title', 'book_genre', 'book_description', 'book_cover_url').then(bookData =>{
+        let bookAndAuthorData = bookData;
+
+        console.log(bookAndAuthorData);
+       response.json(bookAndAuthorData);
    });
+
+
+
+
+    // then get authors
+
+
 });
+
+
+
 
 module.exports = router;
